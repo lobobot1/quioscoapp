a5a09945ae8b2cf7f88f98f9837f02f660679c34
kevin <kevincolmenaresm@gmail.com>
2023-08-25 10:48:05 -0400

    layout y page modificados

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='md:flex'>
+          <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5'>
+            <h1>SideBar aqui</h1>
+          </aside>
+          <main className='md:w-8/12 xl:w3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   )
 }
