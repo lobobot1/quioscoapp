376034870a10618aa2d669da493976a3c6818b24
kevin <kevincolmenaresm@gmail.com>
2023-08-29 15:24:35 -0400

    className de tailwindcss arreglada

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
             <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5'>
               <SideBar />
             </aside>
-            <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 min-h-screen overflow-y-scroll'>
+            <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
               <div className='p-10 mt-2'>
                 {children}
               </div>
