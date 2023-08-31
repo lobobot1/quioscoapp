b35993b94f11a5beef202f118099c43b5a9b275f
kevin <kevincolmenaresm@gmail.com>
2023-08-31 15:26:57 -0400

    agregando un modal al context y funcionalidades extra

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,13 +1,30 @@
 "use client";
 import { useState, useEffect, createContext } from "react";
 import { getCategories } from "../fetch/categoria";
+import Modal from "react-modal";
+import ModalProducto from "../components/ModalProducto";
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('.__className_20951f');
 
 const QuioscoContext = createContext();
 
 function QuioscoProvider({ children }) {
   const [categories, setCategories] = useState([]);
   const [currentCategory, setCurrentCategory] = useState({});
-  const [producto, setProducto] = useState([]);
+  const [producto, setProducto] = useState({});
+  const [modal, setModal] = useState(false);
+  const [pedido, setPedido] = useState([]);
 
   const getCategory = async () => {
     const res = await getCategories();
@@ -23,13 +40,30 @@ function QuioscoProvider({ children }) {
     setCurrentCategory(categories[0]);
   }, [categories]);
 
-  const handleClickCategory = id => {
-    const category = categories.find(category => category.id === id);
+  const handleClickCategory = (id) => {
+    const category = categories.find((category) => category.id === id);
     setCurrentCategory(category);
-    };
-    const handleSetProducto = (product) =>{
-      setProducto(product);
-    };
+  };
+
+  const handleSetProducto = (product) => {
+    setProducto(product);
+  };
+
+  const handleChangeModal = () => {
+    setModal(!modal);
+  };
+
+  const handleAgregarPedido = ({imagen,categoriaId,...producto}) => {
+    if(pedido.some(productoState => productoState.id === producto.id)){
+      const pedidoActualizado = pedido.map(productoState => 
+        productoState.id === producto.id ? producto : productoState
+      );
+      setPedido(pedidoActualizado);
+    }else{
+      setPedido([...pedido, producto]);
+    }
+    handleChangeModal();
+  }
 
   return (
     <QuioscoContext.Provider
@@ -38,9 +72,21 @@ function QuioscoProvider({ children }) {
         currentCategory,
         handleClickCategory,
         handleSetProducto,
+        modal,
+        handleChangeModal,
       }}
     >
       {children}
+      {modal && (
+        <Modal
+          isOpen={modal}
+          onRequestClose={handleChangeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <ModalProducto producto={producto} handleChangeModal={handleChangeModal} handleAgregarPedido={handleAgregarPedido} pedido={pedido}/>
+        </Modal>
+      )}
     </QuioscoContext.Provider>
   );
 }
